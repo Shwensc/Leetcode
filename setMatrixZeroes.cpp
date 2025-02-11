62d089c32366fb6245b073c4bfcacf0ac07730a8
Shwen <shwensc@gmail.com>
2025-02-12 00:06:37 +0530

    LC1-striver update

--- setMatrixZeroes.cpp
+++ setMatrixZeroes.cpp
@@ -4,29 +4,34 @@ using namespace std;
 
 class Solution {
 public:
+    //using 2d dynamic array
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
+        int m = matrix.size(); //no. of rows
+        int n=matrix[0].size();
+        //sets store rows and columns that need to be zeroed
         unordered_set<int> zeroRows;
-        unordered_set<int> zeroCols;
+        unordered_set<int> zeroColumns;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows.insert(i);  // Mark the row
-                    zeroCols.insert(j);  // Mark the column
+        //check which rows and columns need to be zeroed
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    //mark the row and column
+                    zeroRows.insert(i);
+                    zeroColumns.insert(j);
                 }
             }
         }
 
-    // Set the entire row and column to 0 if needed
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (zeroRows.count(i) > 0 || zeroCols.count(j) > 0) {
-                    matrix[i][j] = 0;
-                }
+        //set entire row and column to zero
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(zeroRows.count(i)>0 || zeroColumns.count(j)>0)
+                //count method checks if element there, id there it retgurns 1. so >0 condition used
+                matrix[i][j]=0;
             }
         }
-    }
+
+       
+    };
 };
