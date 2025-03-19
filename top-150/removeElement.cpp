72aff7d2d0f03f109553b609979d362861c188cf
Shwen <shwensc@gmail.com>
2025-03-19 22:04:31 +0530

    remove element thats given and retuen actual valid elements

---
+++ top-150/removeElement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int removeElement(vector<int>& nums, int val) {
+            int i = 0; // Pointer for valid elements
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[j] != val) {
+                    nums[i] = nums[j]; // Overwrite value
+                    i++;  // Move pointer
+                }
+            }
+            return i; // `i`  represents the number of valid elements
+        }
+    };
+    
